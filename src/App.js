6a7c41a763cd0da5aad126785ecefcc65c0c9900
guglani5 <guglani.tech@gmail.com>
2022-09-24 21:58:36 +0530

    Unit test case initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [counter, setCounter] = React.useState(0);
+  const delayCount = () =>
+		setTimeout(() => {
+			setCounter(counter + 1);
+		}, 500);
+
+  const name="Kush Guglani"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section className="myapp">
+    <div>
+      <h1>Hi Hello react {name} </h1>
     </div>
+    <div>
+      <h1>Welcome to unit test case </h1>
+      <p>we learn snapshot component </p>
+    </div>
+    <div>
+      <button onClick={()=>setCounter(counter+1)} data-testid='button-up' >Increment</button>
+      <button onClick={()=>setCounter(counter-1)} data-testid='button-down' >Decrement</button>
+
+      <button onClick={delayCount} data-testid='button-up-inc' >Increment After 5 sec</button>
+
+      <p data-testid="counter">{counter}</p>
+    </div>
+    </section>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,51 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup, fireEvent,waitForElement, } from '@testing-library/react';
 import App from './App';
+afterEach(cleanup);
+describe("testing App snapshot and text",()=>{
+  it("should match a snapshot", () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment(<App />)).toMatchSnapshot();
+  });
+  it('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Hello react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+})
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("testing buttons and content",()=>{
+  it("default counter value should be present",()=>{
+    const { getByTestId } = render(<App />);
+    expect(getByTestId("counter")).toHaveTextContent(0);
+  })
+
+  it("should be enabled", () => {
+    const { getByTestId } = render(<App />);
+    expect(getByTestId("button-up")).not.toHaveAttribute("disabled");
+  });
+  xit("should be disabled", () => {
+    const { getByTestId } = render(<App />);
+    expect(getByTestId("button-down")).toBeDisabled();
+  });
+
+  it("increments counter", () => {
+    const { getByTestId } = render(<App />);
+    fireEvent.click(getByTestId("button-up"));
+    expect(getByTestId("counter")).toHaveTextContent("1");
+  });
+  it("decrements counter", () => {
+    const { getByTestId } = render(<App />);
+    fireEvent.click(getByTestId("button-down"));
+    expect(getByTestId("counter")).toHaveTextContent("-1");
+  });
+
+  it("increments async counter", () => {
+    const { getByTestId } = render(<App />);
+    fireEvent.click(getByTestId("button-up"));
+    const counter = waitForElement(() => getByText("1"));
+    expect(counter).toHaveTextContent("1");
+  });
+
+})
+
+
